0f80135d0047a7d697e0babd57211500a5542e59
kasula pooja <kasulapooja@kasulas-MacBook-Pro.local>
2026-04-30 19:09:48 +0530

    Fix listen syntax

--- backend/src/index.js
+++ backend/src/index.js
@@ -77,4 +77,4 @@ app.use((err, req, res, next) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, '0.0.0.0'() => console.log(`BizManager API running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0',() => console.log(`BizManager API running on port ${PORT}`));
